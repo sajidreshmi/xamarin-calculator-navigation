bf3021acc9977c0a15cc602ac524660a1ab0d0ca
Sajidahmed Reshmi <sajidreshmi@gmail.com>
2022-03-15 19:48:00 +0100

    first commit

---
+++ Navigation/Navigation/Modules/Info/AppInformation/AppInformationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Navigation.Common;
+using Xamarin.Essentials;
+
+namespace Navigation.Modules.AppInformation
+{
+    public class AppInformationViewModel: BaseViewModel
+    {
+        public string AppName => $"App Name: {AppInfo.Name}";
+        public string AppVersion => $"App Version: {AppInfo.VersionString}";
+        public string Author => "Author: Test Author";
+    }
+}
